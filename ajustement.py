5ea673f7be89dc95520065a498425ff06dccc9a6
Jürgens Linux <jurgenstyll@icloud.com>
2025-04-02 20:55:54 +0200

    Ajustement de 3 scores

--- ajustement.py
+++ ajustement.py
@@ -10,15 +10,18 @@ def ajustement(image,user):
     print("notep",notep)
     err=notei-notep
     print("err",err)
-    for key in user.keys():
-        user[key]=user[key]+0.1*err*image[key]
-        print(user[key])
+    for key in image.keys():
+        user[key]=round(float(user[key]+0.01*err*image[key]),2)
+        if user[key]<0:
+            user[key]=0
+        if user[key]>5:
+            user[key]=5
     return user
 
-dico = {"animal":2,"architectural":2.5,"biodiversité":5,"aquatique":1,"nourriture":3.4,"astres":1,"forestier":0.5,"art":0,"informatique":0,"vidéo":0,"sport":0,"véhicule":0,"manga":0,"fleur":0}
+user = {"animal":2,"architectural":2.5,"biodiversité":5,"aquatique":1,"nourriture":3.4,"astres":1,"forestier":0.5,"art":0,"informatique":0,"vidéo":0,"sport":0,"véhicule":0,"manga":0,"fleur":0}
 
-user = {"animal":0,"architectural":0,"biodiversité":0,"aquatique":0,"nourriture":0,"astres":0,"forestier":0,"art":0,"informatique":0,"vidéo":0,"sport":0,"véhicule":0,"manga":0,"fleur":0}
-
-user = ajustement(dico,user)
+dicotest={"animal":5,"architectural":4,"biodiversité":5}
+dico = {'animal': 0, 'architectural': 5, 'biodiversité': 2}
+user = ajustement(dicotest,user)
 
 print(user)
