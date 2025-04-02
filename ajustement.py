ef561d646bac5fe524f4fc4cf17c5cc393e1545c
Jürgens Linux <jurgenstyll@icloud.com>
2025-04-02 19:48:50 +0200

    Update ajustement.py

---
+++ ajustement.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def ajustement(image,user):
+    return user
+
+dico = {"animal":2,"architectural":2.5,"biodiversité":5,"aquatique":1,"nourriture":3.4,"astres":1,"forestier":0.5,"art":0,"informatique":0,"vidéo":0,"sport":0,"véhicule":0,"manga":0,"fleur":0}
+
+user = {"animal":0,"architectural":0,"biodiversité":0,"aquatique":0,"nourriture":0,"astres":0,"forestier":0,"art":0,"informatique":0,"vidéo":0,"sport":0,"véhicule":0,"manga":0,"fleur":0}
+
+user = ajustement(dico,user)
