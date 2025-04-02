bec5bd2771965275386743abc1f6be89223c52f0
Jürgens Linux <jurgenstyll@icloud.com>
2025-04-02 20:09:33 +0200

    Ajustement de tous les scores

--- ajustement.py
+++ ajustement.py
@@ -1,6 +1,18 @@
 import numpy as np
 
 def ajustement(image,user):
+
+    valeursi=list(image.values())
+    valeursu=list(user.values())
+    notei=np.mean(valeursi)
+    notep = np.mean(valeursu)
+    print("notei",notei)
+    print("notep",notep)
+    err=notei-notep
+    print("err",err)
+    for key in user.keys():
+        user[key]=user[key]+0.1*err*image[key]
+        print(user[key])
     return user
 
 dico = {"animal":2,"architectural":2.5,"biodiversité":5,"aquatique":1,"nourriture":3.4,"astres":1,"forestier":0.5,"art":0,"informatique":0,"vidéo":0,"sport":0,"véhicule":0,"manga":0,"fleur":0}
@@ -8,3 +20,5 @@ dico = {"animal":2,"architectural":2.5,"biodiversité":5,"aquatique":1,"nourritu
 user = {"animal":0,"architectural":0,"biodiversité":0,"aquatique":0,"nourriture":0,"astres":0,"forestier":0,"art":0,"informatique":0,"vidéo":0,"sport":0,"véhicule":0,"manga":0,"fleur":0}
 
 user = ajustement(dico,user)
+
+print(user)
