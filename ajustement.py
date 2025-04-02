19a879bec2dcf0d5cc00a9ff5b147e592cb953c9
Jürgens Linux <jurgenstyll@icloud.com>
2025-04-02 21:00:42 +0200

    Update ajustement.py

--- ajustement.py
+++ ajustement.py
@@ -6,16 +6,11 @@ def ajustement(image,user):
     valeursu=list(user.values())
     notei=np.mean(valeursi)
     notep = np.mean(valeursu)
-    print("notei",notei)
-    print("notep",notep)
     err=notei-notep
-    print("err",err)
     for key in image.keys():
         user[key]=round(float(user[key]+0.01*err*image[key]),2)
-        if user[key]<0:
-            user[key]=0
-        if user[key]>5:
-            user[key]=5
+        user[key]=0 if user[key]<0 else user[key]
+        user[key]=5 if user[key]>5 else user[key]
     return user
 
 user = {"animal":2,"architectural":2.5,"biodiversité":5,"aquatique":1,"nourriture":3.4,"astres":1,"forestier":0.5,"art":0,"informatique":0,"vidéo":0,"sport":0,"véhicule":0,"manga":0,"fleur":0}
